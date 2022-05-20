c83b837a2a772d99d2303a15bc0b68ececf44f0d
Dalmasio <dalmasiof@gmail.com>
2022-05-19 21:15:47 -0300

    ajuste

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -7,16 +8,14 @@ app.use("/styles", express.static(__dirname));
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
 
-  app.use('/',express.static('dist/coverALL'))
-
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/coverALL/index.html'));
 });
 
 // add other routes below
-// app.get('/about', function (req, res) {
-//   res.sendFile(path.join(__dirname + 'views/about.html'));
-// });
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname + 'views/about.html'));
+});
 
 app.listen(process.env.PORT || 8080);
