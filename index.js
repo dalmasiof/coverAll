3df20167eb6151388cd17e6f5136a7936e9ba9b5
Dalmasio <dalmasiof@gmail.com>
2022-05-19 23:21:25 -0300

    asd

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ app.use(express.static(__dirname + '/dist/coverALL'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/coverALL/index.html'));});
-app.listen(process.env.PORT || 8080);
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
