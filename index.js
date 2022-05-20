0ce9134e5953deda9bb89e0301982882891b088d
Dalmasio <dalmasiof@gmail.com>
2022-05-19 23:09:25 -0300

    asa

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + 'dist/coverALL'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/coverALL/index.html'));});
 app.listen(process.env.PORT || 8080);
 
