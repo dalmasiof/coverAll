a11b3a8ba3ceebb95fc68ff49ea6f0520e504a51
Dalmasio <dalmasiof@gmail.com>
2022-05-21 12:45:46 -0300

    teste getProd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'coverALL';
+export class AppComponent implements OnInit {
+  /**
+   *
+   */
+  title = 'Coverr All';
+  
+  constructor(private httpSvc: HttpClient) {}
+  
+  ngOnInit(): void {
+    debugger;
+    let url = 'https://coverall-api.herokuapp.com/produto';
+    this.httpSvc.get(url).subscribe((x) => {
+      console.log(x);
+    });
+  }
+  
 }
