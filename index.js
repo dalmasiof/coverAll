9984ff658fb754a556781e074d217cd5a927c76b
Dalmasio <dalmasiof@gmail.com>
2022-05-21 14:19:15 -0300

    volta

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
-
 const path = require('path');
-const Webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-/** this is custom js to help proxy in server.js*/
-const webpackConfig = require('./webpack.config.js');
-
-
-const compiler = Webpack(webpackConfig);
-const devServerOptions = { ...webpackConfig.devServer, open: true };
-const server = new WebpackDevServer(devServerOptions, compiler);
-
-const runServer = async () => {
-  console.log('Starting server...');
-  await server.start();
-};
-
-runServer();
-/** If you have error creating proxy <app-url> to localhost  
- * Heroku internally redirect the Server port 8080 .
- *  For that reason we need to open listener  port(I used 3000 here) redirect 
-through http-proxy-middleware*/
- app.use("/*", createProxyMiddleware(
-    { target: "https://localhost:8080", 
-    ws: true ,
-     changeOrigin: true,
-     secure:false,
-     router: {
-    'dev.localhost:3000': 'https://localhost:8080',
-  },})) 
-
-app.listen(process.env.PORT || 3000)
-
+const app = express();
 app.use(express.static(__dirname + '/dist/coverALL'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/coverALL/index.html'));});
-app.listen(process.env.PORT || 4000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Node app is running at localhost:" + app.get('port'));
 });
