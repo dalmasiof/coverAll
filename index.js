00056a678d7ea43650e4013f011b5640f542aa8d
Dalmasio <dalmasiof@gmail.com>
2022-05-21 13:32:32 -0300

    teste

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.use(express.static(__dirname + '/dist/coverALL'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/coverALL/index.html'));});
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("Node app is running at localhost:" + app.get('port'));
 });
