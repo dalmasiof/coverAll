2c39455ce30102585c05928843a052a7de21498d
Dalmasio <dalmasiof@gmail.com>
2022-05-21 14:27:14 -0300

    retirar proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,10 @@ export class AppComponent implements OnInit {
   
   ngOnInit(): void {
     debugger;
-    let url = 'api/produto';
+    let url = 'https://coverall-api.herokuapp.com/produto';
     this.httpSvc.get(url).subscribe((x) => {
       console.log(x);
     });
   }
-
+  
 }
