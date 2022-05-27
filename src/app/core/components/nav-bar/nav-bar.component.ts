e89e302cf640af85cea55d138bc65067aa28f480
Dalmasio <dalmasiof@gmail.com>
2022-05-26 21:30:38 -0300

    COntrole de cliente apenas para admin

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from 'src/app/shared/Services/LocalStorage/local-storage.service';
 import { LoggedUserService } from 'src/app/core/services/loggedUser/logged-user.service';
+import { Cliente } from '../../model/Cliente';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,11 +10,15 @@ import { LoggedUserService } from 'src/app/core/services/loggedUser/logged-user.
 })
 export class NavBarComponent implements OnInit {
   isLogged:boolean = false
+  cliente:Cliente | undefined
 
   constructor(private localStorageSvc:LocalStorageService, private loggedUser:LoggedUserService
     ) {
+      
       this.loggedUser.getValue().subscribe((x)=>{
         debugger;
+        if(this.localStorageSvc.getValue('cliente'))
+        this.cliente = this.localStorageSvc.getValue('cliente');
         this.isLogged = x})
      }
 
